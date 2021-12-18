a5c0709a0047f50154601512b5556da7bb98acd3
sdtuapse <sergiodrovski@gmail.com>
2021-12-19 00:04:38 +0300

    добавлена корзина

--- lesson_7/public/assets/js/personalScript.js
+++ lesson_7/public/assets/js/personalScript.js
@@ -12,3 +12,20 @@ for (let i = 0; i < elemRatingAll.length; i++) {
         collect[i].classList.add('review-fill');
     }
 }
+
+// Расчёт корзины
+//Боковая
+const Basket = document.querySelectorAll(`.offcanvas-cart-item-details`);
+let totalSum = 0;
+for (let i = 0; i < Basket.length; i++) {
+    let price = Basket[i].children[1].dataset.price;
+    let quantity = Basket[i].children[0].dataset.quantity;
+    let elemSum = price * quantity;
+    totalSum += elemSum;
+}
+document.querySelector('.offcanvas-cart-total-price-value').innerHTML = totalSum + ' руб.';
+
+//на основной странице
+document.querySelector('.basket_price').innerHTML = totalSum + ' руб.';
+let shipPrice = document.querySelector('.ship_price').dataset.ship;
+document.querySelector('.total_price').innerHTML = totalSum + Number(shipPrice) + ' руб.';
